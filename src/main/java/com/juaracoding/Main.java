5645d6668f8b5e205e7406bd5eca9b46d5a37221
IhlasulMufti <ihlasul05mufti@gmail.com>
2024-09-26 22:26:25 +0800

    Java Fundamental First Day

---
+++ src/main/java/com/juaracoding/Main.java
@@ -0,0 +1,18 @@
+package com.juaracoding;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Ready for SQA RPA");
+        int x = 100;
+
+        if(true){
+            System.out.println(x);
+        }
+
+        if(true){
+            System.out.println(x);
+        }
+    }
+}
